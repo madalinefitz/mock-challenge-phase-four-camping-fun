aeae973fb10f68d40882214b178d220694275cb4
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-04-11 22:27:19 -0400

    lab completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from sqlalchemy.exc import IntegrityError
 
 from models import db, Camper, Activity, Signup
 
@@ -12,20 +14,115 @@ app.json.compact = False
 CORS(app)
 migrate = Migrate(app, db)
 
+
 db.init_app(app)
 
-# api = Api(app)
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    response = make_response(
-        {
-            "message": "Hello Campers!"
-        },
-        200
-    )
-    return response
+class Campers(Resource):
+    def get(self):
+        campers = [c.to_dict() for c in Camper.query.all()]
+        return make_response(campers, 201)
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_camper = Camper(name = data['name'], age = data['age'])
+            db.session.add(new_camper)
+            db.session.commit()
+        
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({'errors': '[validation errors]'}, 400)
+
+        return make_response(new_camper.to_dict(), 201)
+
+api.add_resource(Campers, "/campers")
+
+class CampersId(Resource):
+    def get(self, id):
+        camper = Camper.query.filter_by(id=id).first()
+
+        if camper == None:
+            return make_response({'error': 'camper not found'}, 400)
+
+        return make_response(camper.to_dict(only = ('name', 'id', 'age', 'activity')), 201)
+
+    def patch(self, id):
+        camper = Camper.query.filter_by(id=id).first()
+        data = request.get_json()
+
+        try:
+            for key in data.keys():
+                setattr(camper, key, data[key])
+
+            db.session.add(camper)
+            db.session.commit()
+        
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({'error' : 'invalid data'})
+        
+        return make_response(camper.to_dict(), 201)
+
+
+api.add_resource(CampersId, "/campers/<int:id>")
+
+class Activities(Resource):
+    def get(self):
+        activities = [a.to_dict() for a in Activity.query.all()]
+        return make_response(activities, 201)
+
+api.add_resource(Activities, "/activities")
+
+class ActivitiesId(Resource):
+    def get(self, id):
+        activity = Activity.query.filter_by(id=id).first()
+
+        if activity == None:
+            return make_response({'error' : 'activity not found'}, 400)
+
+        return make_response(activity.to_dict(), 201)
+    
+    def delete(self, id):
+        
+        for s in Signup.query.all():
+            if s.activity_id == id:
+                db.session.delete(s)
+                db.session.commit()
+        
+        activity = Activity.query.filter_by(id=id).first()
+        
+        if activity == None:
+            return make_response({'error' : 'activity not found'})
+
+        db.session.delete(activity)
+        db.session.commit()
+
+        return make_response({}, 200)
+
+api.add_resource(ActivitiesId, "/activities/<int:id>")
+
+
+class Signups(Resource):
+    def get(self):
+        signups = [s.to_dict() for s in Signup.query.all()]
+        return make_response(signups, 201)
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_signup = Signup(time = data['time'], camper_id = data['camper_id'], activity_id = data['activity_id'])
+            db.session.add(new_signup)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({"errors": ["validation errors"]}, 400)
+
+        return make_response(new_signup.activity.to_dict(), 200)
+
+api.add_resource(Signups, '/signups')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -9,16 +9,47 @@ db = SQLAlchemy()
 class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ('-signup', '-created_at', '-updated_at')
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    age = db.Column(db.Integer, db.CheckConstraint( '(age >= 8) and (age <= 18)' ))
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    __table_args__ = ( db.CheckConstraint('(age >= 8) and (age <= 18)'), )
+
+    signup = db.relationship('Signup', back_populates = 'camper')
+    activity = association_proxy('signup', 'activity')
 
 
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ('-signup', '-created_at', '-updated_at' )
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    difficulty = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    signup = db.relationship('Signup', back_populates = 'activity')
+    camper = association_proxy('signup', 'camper')
 
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+    serialize_rules = ('-activity', '-camper')
+
     id = db.Column(db.Integer, primary_key=True)
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+    time = db.Column(db.Integer, db.CheckConstraint('(time >= 1) and (time <= 23)'))
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    __table_args__ = (db.CheckConstraint('(time >= 1) and (time <= 23)'), )
+
+    activity = db.relationship('Activity', back_populates = 'signup')
+    camper = db.relationship('Camper', back_populates = 'signup')
+    
